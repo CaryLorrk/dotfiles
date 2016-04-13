2c900a86c5923b3fd3b0a9f6ed166affce3ffe66
carylorrk <carylorrk@gmail.com>
2016-04-14 01:52:09 +0800

    burn_subtitle

--- bash/plugins/video.plugin.bash
+++ bash/plugins/video.plugin.bash
@@ -7,4 +7,12 @@ concat_video() {
 }
 export -f concat_video
 
+burn_subtitle() {
+    if [ -z "$1"] || [ -z "$2" ] || [ -z "$3" ]; then
+        echo "usage: burn_subtitle video subtitle output"
+        return
+    fi
 
+    echo_and_run "ffmpeg -i \"$1\" -vf subtitles=\"$2\" -map 0:v -map 0:a -c:a copy -c:v libx264 -crf 20 \"$3\""
+}
+export -f burn_subtitle
