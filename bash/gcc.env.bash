a50b58e88e1b5ae3f770aea257a463564672cfd4
carylorrk <carylorrk@gmail.com>
2016-04-24 06:41:42 +0800

    gcc.env.bash

--- bash/gcc.env.bash
+++ bash/gcc.env.bash
@@ -1,2 +1,2 @@
 WCFLAGS="-Wall -Wextra -pedantic -fdiagnostics-color=auto -Wfloat-equal -Wundef -Wshadow -Wpointer-arith -Wcast-align -Wcast-qual -Wwrite-strings -Wformat=2 -Winit-self -Wlogical-op -Wunreachable-code -Wredundant-decls -Wswitch-default -Wswitch-enum -Wconversion -Wsign-conversion -Wstrict-overflow=5 -Wdisabled-optimization"
-WCXXFLAGS="$WCOMMONFLAGS -Wctor-dtor-privacy -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wsign-promo -Wstrict-null-sentinel -Weffc++"
+WCXXFLAGS="$WCFLAGS -Wctor-dtor-privacy -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wsign-promo -Wstrict-null-sentinel -Weffc++"
